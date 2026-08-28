daef36711dafd93526e2d72df8a52c0a5fa67e19
Jeff Emanuel <jeff141421@gmail.com>
2026-08-28 03:30:36 -0400

    test(storage): cover comment and overflow preservation (#461) [skip ci]

--- tests/e2e_concurrency.rs
+++ tests/e2e_concurrency.rs
@@ -3112,7 +3112,7 @@ fn e2e_parallel_writes_preserve_large_description_and_freelist() {
                                 &root,
                                 [
                                     "--lock-timeout",
-                                    "15000",
+                                    "60000",
                                     "update",
                                     &issue_id,
                                     "--notes",
@@ -3125,7 +3125,7 @@ fn e2e_parallel_writes_preserve_large_description_and_freelist() {
                                 &root,
                                 [
                                     "--lock-timeout",
-                                    "15000",
+                                    "60000",
                                     "comments",
                                     "add",
                                     &issue_id,
